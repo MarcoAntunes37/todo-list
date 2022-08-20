bb0bab03e6bdd1fd2fd26af266ff3cbb859a35e5
Marco Aurelio <marcoantunesdev@gmail.com>
2022-08-19 23:00:31 -0300

    layout adjust

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import TodoList from './Component/TodoList';
 function App() {  
    return (
     <div className="App">
-      <div className='app-container'>
         <div className='form-container'>  
           <Form/>
         </div>
         <div className='content-container'>
           <TodoList/>
         </div>
-      </div>
     </div>
   );
 }
